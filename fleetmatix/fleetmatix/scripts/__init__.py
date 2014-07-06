506df487fb076dc687297edc29d4b8ee4a1d5a33
Ghazala Majeed <magz_bsse@yahoo.com>
2014-07-07 04:38:22 +0500

    intrm changes

--- fleetmatix/fleetmatix/scripts/__init__.py
+++ fleetmatix/fleetmatix/scripts/__init__.py
@@ -1,1 +1,16 @@
-# package
+from sqlalchemy.ext.declarative import declarative_base
+from zope.sqlalchemy import ZopeTransactionExtension
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+
+db = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
+#from models import your_model_names_here
+from auth import Permission, User, UserPermission, RoutePermission
+from models import Station
+
+# Place additional model names here for ease of importing.
+__all__ = ['db', 'Base', 'Permission', 'User', 'UserPermission', 'RoutePermission', 'Station']
