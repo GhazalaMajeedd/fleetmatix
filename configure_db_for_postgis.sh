82da28c98726475742c968f91c5974cf01ab96e9
Kashif Iftikhar <kashif@compulife.com.pk>
2014-07-24 08:09:17 +0500

    Updated postgis configure and unconfigure scripts to allow specifying version numbers of postgresql and postgis

--- configure_db_for_postgis.sh
+++ configure_db_for_postgis.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
+POSTGRES_VERSION=9.1
+POSTGIS_VERSION=2.0
+
 if [ -z $1 ]; then
-    echo "Please provide database name"
+    echo "Syntax:\n\t $0 dbname [postgres_version] [postgis_version]\n\nExample:\n\t $0 fleetmatix 9.1 2.0"
     exit
 fi
 
+if [ -n $2 ]; then
+    POSTGRES_VERSION=$2
+fi
+
+if [ -n $3 ]; then
+    POSTGIS_VERSION=$3
+fi
+
+echo $POSTGRES_VERSION
+echo $POSTGIS_VERSION
+
 createlang plpgsql $1
-psql -d $1 -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis.sql
-psql -d $1 -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis_comments.sql
-psql -d $1 -f /usr/share/postgresql/9.1/contrib/postgis-2.0/spatial_ref_sys.sql
-psql -d $1 -f /usr/share/postgresql/9.1/contrib/postgis-2.0/legacy.sql
-psql -d $1 -f /usr/share/postgresql/9.1/contrib/postgis-2.0/rtpostgis.sql
+psql -d $1 -f /usr/share/postgresql/$POSTGRES_VERSION/contrib/postgis-$POSTGIS_VERSION/postgis.sql
+psql -d $1 -f /usr/share/postgresql/$POSTGRES_VERSION/contrib/postgis-$POSTGIS_VERSION/postgis_comments.sql
+psql -d $1 -f /usr/share/postgresql/$POSTGRES_VERSION/contrib/postgis-$POSTGIS_VERSION/spatial_ref_sys.sql
+psql -d $1 -f /usr/share/postgresql/$POSTGRES_VERSION/contrib/postgis-$POSTGIS_VERSION/legacy.sql
+psql -d $1 -f /usr/share/postgresql/$POSTGRES_VERSION/contrib/postgis-$POSTGIS_VERSION/rtpostgis.sql
 
--- unconfigure_db_for_postgis.sh
+++ unconfigure_db_for_postgis.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+POSTGRES_VERSION=9.1
+POSTGIS_VERSION=2.0
+
 if [ -z $1 ]; then
-    echo "Please provide database name"
+    echo "Syntax:\n\t $0 dbname [postgres_version] [postgis_version]\n\nExample:\n\t $0 fleetmatix 9.1 2.0"
     exit
 fi
 
-psql -d $1 -f /usr/share/postgresql/9.1/contrib/postgis-1.5/uninstall_postgis.sql
+if [ -n $2 ]; then
+    POSTGRES_VERSION=$2
+fi
+
+if [ -n $3 ]; then
+    POSTGIS_VERSION=$3
+fi
+
+echo $POSTGRES_VERSION
+echo $POSTGIS_VERSION
+
+
+psql -d $1 -f /usr/share/postgresql/$POSTGRES_VERSION/contrib/postgis-$POSTGIS_VERSION/uninstall_postgis.sql
 
